4aead296a99897ee729126a4c95dae28341a040c
ATMAKURI KRISHNASAI BHARADWAJ <34579499+AKSB0567@users.noreply.github.com>
2024-02-20 13:28:44 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
+#from fastapi import FastAPI
 
+#app = FastAPI()
+
+
+#@app.get("/")
+#async def root():
+#    return {"message": "Hello World",
+#            "group": "4"
+#           }
+
+# main.py
+
+# main.py
+
+from fastapi import FastAPI
+from routes.route import router
+
+app = FastAPI()
+
+app.include_router(router)
